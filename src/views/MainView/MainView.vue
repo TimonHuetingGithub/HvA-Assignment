792e1f03eca4d3feb3706ab8889ecfc304cd3874
Timon <t.hueting@atabix.nl>
2024-02-04 21:01:41 +0100

    First commit

---
+++ src/views/MainView/MainView.vue
@@ -0,0 +1,11 @@
+<!-- Base URL -->
+<!-- https://swapi.dev/api/ -->
+
+<!-- SWAPI Docs -->
+<!-- https://swapi.dev/documentation -->
+
+<script lang="ts" setup></script>
+
+<template>
+  <div>Main view</div>
+</template>
